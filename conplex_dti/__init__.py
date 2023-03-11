0b6b2875597487ca5e2718c377934e94de493e6e
samsledje <samsledje@gmail.com>
2023-03-11 12:16:44 -0500

    Initial commit

---
+++ conplex_dti/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Adapting protein language models and contrastive learning for DTI prediction."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
---
+++ conplex_dti/__main__.py
@@ -0,0 +1,67 @@
+# type: ignore[attr-defined]
+from typing import Optional
+
+from enum import Enum
+from random import choice
+
+import typer
+from rich.console import Console
+
+from conplex_dti import version
+from conplex_dti.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="conplex-dti",
+    help="Adapting protein language models and contrastive learning for DTI prediction.",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(print_version: bool) -> None:
+    """Print the version of the package."""
+    if print_version:
+        console.print(f"[yellow]conplex-dti[/] version: [bold blue]{version}[/]")
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for print. If not specified then choice will be random.",
+    ),
+    print_version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the conplex-dti package.",
+    ),
+) -> None:
+    """Print a greeting with a giving name."""
+    if color is None:
+        color = choice(list(Color))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
+
+
+if __name__ == "__main__":
+    app()
