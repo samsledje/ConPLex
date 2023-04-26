01850967c242218c7ffed2d985813a35b4031067
samsledje <samsledje@gmail.com>
2023-04-26 17:44:17 -0400

    update config and docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,16 +6,16 @@
 import os.path
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 import conplex_dti
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'conplex-dti'
-copyright = '2023, Samuel Sledzieski, Rohit Singh'
-author = 'Samuel Sledzieski, Rohit Singh'
-release = '0.1.0'
+project = "conplex-dti"
+copyright = "2023, Samuel Sledzieski, Rohit Singh"
+author = "Samuel Sledzieski, Rohit Singh"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,16 +24,15 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
