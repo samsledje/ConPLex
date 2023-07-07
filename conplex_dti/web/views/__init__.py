e7a577f7c5feb61dd7cad1583deeb9c05f657c0a
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-07 11:15:19 -0400

    Add blueprint registration and the index view.

--- conplex_dti/web/views/__init__.py
+++ conplex_dti/web/views/__init__.py
@@ -5,3 +5,12 @@ def register_views(app: flask.Flask) -> None:
     """
     Register the application's views.
     """
+
+    blueprints = []
+
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
+
+    @app.route("/")
+    def index():
+        return "Index"
