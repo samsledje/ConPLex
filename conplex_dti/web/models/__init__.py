4c464bfd5b1bd9411485fda923035fc0e868fd83
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-07 11:07:40 -0400

    Add the database session and base model class.

--- conplex_dti/web/models/__init__.py
+++ conplex_dti/web/models/__init__.py
@@ -1,3 +1,9 @@
+from .base import Model as Model
+from .base import db_engine as db_engine
+from .base import db_session as db_session
+
+# All models.
+
 import flask
 
 
@@ -5,3 +11,11 @@ def register_models(app: flask.Flask) -> None:
     """
     Register the application's models.
     """
+
+    @app.teardown_appcontext
+    def remove_db_session(_=None):
+        """
+        Remove the database session after a request.
+        See https://docs.sqlalchemy.org/en/20/orm/contextual.html.
+        """
+        db_session.remove()
---
+++ conplex_dti/web/models/base.py
@@ -0,0 +1,26 @@
+import flask
+import fsw.models
+import sqlalchemy
+import sqlalchemy.orm
+
+db_engine = sqlalchemy.create_engine(flask.current_app.config("DATABASE_URL"))
+db_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(bind=db_engine))
+
+
+class Model(
+    sqlalchemy.orm.DeclarativeBase,
+    fsw.models.ClassNameModelMixin,
+    fsw.models.IDModelMixin,
+    fsw.models.SaveModelMixin,
+    fsw.models.CreateTimestampModelMixin,
+    fsw.models.UpdateTimestampModelMixin,
+    fsw.models.DeleteTimestampModelMixin,
+):
+    """
+    The base model class.
+    """
+
+    __abstract__ = True
+
+    # The database session for FSW.
+    database_session = db_session
