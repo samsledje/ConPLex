fe3f4649d48fe76722ecd116ab88a84f17cc2a99
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-08 16:10:51 -0400

    Add configuration variables for email-sending.

--- conplex_dti/web/__init__.py
+++ conplex_dti/web/__init__.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 
 import flask
@@ -15,6 +16,13 @@ def create_app() -> flask.Flask:
         SECRET_KEY=secrets.token_hex(64),
         # TODO: For deployment, use PostgreSQL.
         DATABASE_URL="sqlite:///web.sqlite3",
+        EMAIL_SMTP_SERVER_URL="smtp.gmail.com",
+        # Port 587 is for TLS.
+        EMAIL_SMTP_PORT=587,
+        # For Gmail, the SMTP username equals the sender address.
+        EMAIL_SMTP_USERNAME=os.environ.get("EMAIL_SMTP_USERNAME"),
+        EMAIL_SMTP_PASSWORD=os.environ.get("EMAIL_SMTP_PASSWORD"),
+        EMAIL_SENDER_ADDRESS=os.environ.get("EMAIL_SENDER_ADDRESS"),
     )
 
     with app.app_context():
