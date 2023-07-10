88c8d6c36f74b2c4642f46c3e51885227bb70421
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-10 09:38:34 -0400

    Import and register the `users` blueprint.

--- conplex_dti/web/views/__init__.py
+++ conplex_dti/web/views/__init__.py
@@ -1,12 +1,16 @@
 import flask
 
+from . import users
+
 
 def register_views(app: flask.Flask) -> None:
     """
     Register the application's views.
     """
 
-    blueprints = []
+    blueprints = [
+        users.bp,
+    ]
 
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
