dffee8e98454fd149f4b8ccbb386be14636af782
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-10 09:36:02 -0400

    Use Outlook for sending email from the server.
    Connecting to Gmail with SMTP is difficult.

--- conplex_dti/web/__init__.py
+++ conplex_dti/web/__init__.py
@@ -15,13 +15,13 @@ def create_app() -> flask.Flask:
         SECRET_KEY=os.environ.get("SECRET_KEY"),
         # TODO: For deployment, use PostgreSQL.
         DATABASE_URL="sqlite:///web.sqlite3",
-        EMAIL_SMTP_SERVER_URL="smtp.gmail.com",
+        EMAIL_SMTP_SERVER_URL="smtp-mail.outlook.com",
         # Port 587 is for TLS.
         EMAIL_SMTP_PORT=587,
         # For Gmail, the SMTP username equals the sender address.
-        EMAIL_SMTP_USERNAME=os.environ.get("EMAIL_SMTP_USERNAME"),
+        EMAIL_SMTP_USERNAME="conplex.web@outlook.com",
         EMAIL_SMTP_PASSWORD=os.environ.get("EMAIL_SMTP_PASSWORD"),
-        EMAIL_SENDER_ADDRESS=os.environ.get("EMAIL_SENDER_ADDRESS"),
+        EMAIL_SENDER_ADDRESS="conplex.web@outlook.com",
     )
 
     with app.app_context():
