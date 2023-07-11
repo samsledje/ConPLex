e11ad2d8ab68058aaf03d47820cb465cbcf2455d
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-11 10:44:04 -0400

    Sort imports.

--- conplex_dti/web/models/__init__.py
+++ conplex_dti/web/models/__init__.py
@@ -5,11 +5,11 @@ from .base import db_engine as db_engine
 from .base import db_session as db_session
 
 # All models (for imports and Alembic).
-from .users import User as User
-from .sets import Set as Set
-from .sets import DrugSet as DrugSet
-from .sets import TargetSet as TargetSet
 from .pairings import Pairing as Pairing
+from .sets import DrugSet as DrugSet
+from .sets import Set as Set
+from .sets import TargetSet as TargetSet
+from .users import User as User
 
 
 def register_models(app: flask.Flask) -> None:
