243df57dbc4bd2b6da40667c8ca4193c818a7d99
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-11 11:31:35 -0400

    Add an `index` view for the drug and target sets.

--- conplex_dti/web/views/sets.py
+++ conplex_dti/web/views/sets.py
@@ -3,9 +3,33 @@ Uploading and reviewing drug and target sets.
 """
 
 import flask
+import sqlalchemy
+
+from .. import decorators, models
 
 bp = flask.Blueprint(
     "sets",
     __name__,
     url_prefix="/sets",
 )
+
+
+@bp.route("/")
+@decorators.user_required
+def index():
+    drug_sets = models.db_session.scalars(
+        sqlalchemy.select(models.DrugSet).where(
+            models.DrugSet.user_id == flask.g.user.id
+        )
+    ).all()
+    target_sets = models.db_session.scalars(
+        sqlalchemy.select(models.TargetSet).where(
+            models.TargetSet.user_id == flask.g.user.id
+        )
+    ).all()
+
+    return flask.render_template(
+        "sets/index.html.jinja",
+        drug_sets=drug_sets,
+        target_sets=target_sets,
+    )
