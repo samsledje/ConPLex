d643ef422a29326ae2681dff379ee8afbcfa764a
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-19 17:57:44 -0400

    Create the app context when needed for `tasks` exports.
    When the Huey consumer imports the task queue object,
    an app context does not exist by default.

--- conplex_dti/web/tasks/__init__.py
+++ conplex_dti/web/tasks/__init__.py
@@ -1,1 +1,9 @@
-from .base import task_queue
+import flask
+
+if flask.current_app:
+    from .base import task_queue
+else:
+    from .. import create_app
+
+    with create_app().app_context():
+        from .base import task_queue
