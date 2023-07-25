edd7bb8f79847f22b81a61238348df01497dfa9c
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-25 15:21:36 -0400

    Add element defaults and a function for setting element content.

--- conplex_dti/web/static/js/pairings/visualization.js
+++ conplex_dti/web/static/js/pairings/visualization.js
@@ -11,3 +11,28 @@
 // a `span` element with ID `status`
 // a `span` element with ID `prediction`
 // a relatively positioned `div` element with ID `visualization`
+
+const elementIdToDefault = {
+    "selected-drug": "No drug selected.",
+    "selected-target": "No target selected.",
+    "drugs": "<li>No drugs loaded.</li>",
+    "targets": "<li>No targets loaded.</li>",
+    "status": "Building visualization.",
+    "prediction": "No drug or no target selected."
+}
+
+// `content` is an optional string.
+function set(elementId, content) {
+    if (elementId in elementIdToDefault) {
+        document.getElementById(elementId).innerHTML = (
+            content ? content : elementIdToDefault[elementId]
+        )
+    } else {
+        throw new Error()
+    }
+}
+
+// TODO: Consider creating a `main` function.
+for (let elementId in elementIdToDefault) {
+    set(elementId, null)
+}
