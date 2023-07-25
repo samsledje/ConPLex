6973aacda6abe656b9e801746f99293cd0899b16
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2023-07-25 14:11:29 -0400

    Replace the `canvas` with a generic `div` and add descriptive elements.

--- conplex_dti/web/static/js/pairings/visualization.js
+++ conplex_dti/web/static/js/pairings/visualization.js
@@ -3,9 +3,14 @@
 // `target_projections_url`
 // `predictions_url`
 
-// The caller must have a `canvas` element with ID `canvas`.
-
-// The caller must have a `span` element with ID `status`.
+// The caller must have the following elements:
+// a `span` element with ID `selected-drug`
+// a `span` element with ID `selected-target`
+// a `ul` element with ID `drugs`
+// a `ul` element with ID `targets`
+// a `span` element with ID `status`
+// a `span` element with ID `prediction`
+// a relatively positioned `div` element with ID `visualization`
 
 const statusElement = document.getElementById("status")
 function setStatus(status) {
